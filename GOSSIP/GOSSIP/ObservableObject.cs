cea540f8556549a457d8acd1f97b044e3970b6cc
GARDAR <81230062+GAR-DAR@users.noreply.github.com>
2024-10-05 17:54:45 +0300

    Add files via upload

---
+++ GOSSIP/GOSSIP/ObservableObject.cs
@@ -0,0 +1,15 @@
+﻿
+using System.ComponentModel;
+
+namespace CHASER_2._0
+{
+    public class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
