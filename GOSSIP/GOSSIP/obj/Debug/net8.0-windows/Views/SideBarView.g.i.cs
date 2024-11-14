26ff44b698d7f1e844c35dc8fc946b2489952a7a
IvanOliinyk <02ivanivan02@gmail.com>
2024-11-14 16:37:36 +0200

    Icons added

--- GOSSIP/GOSSIP/obj/Debug/net8.0-windows/Views/SideBarView.g.i.cs
+++ GOSSIP/GOSSIP/obj/Debug/net8.0-windows/Views/SideBarView.g.i.cs
@@ -53,7 +53,7 @@ namespace GOSSIP.Views {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/GOSSIP;V1.0.0.0;component/views/sidebarview.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/GOSSIP;component/views/sidebarview.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\SideBarView.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
