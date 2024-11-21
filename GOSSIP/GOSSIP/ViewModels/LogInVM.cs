913518145670ccfae5f330162e755d27014e6466
pupsaik <151430709+pupsaik@users.noreply.github.com>
2024-11-21 23:17:24 +0200

    test

--- GOSSIP/GOSSIP/ViewModels/LogInVM.cs
+++ GOSSIP/GOSSIP/ViewModels/LogInVM.cs
@@ -5,9 +5,11 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
+using System.Xml;
 
 namespace GOSSIP.ViewModels
 {
@@ -51,7 +53,21 @@ namespace GOSSIP.ViewModels
             var jsonString = File.ReadAllText("user_data.json");
             List<UserModel> users = JsonSerializer.Deserialize<List<UserModel>>(jsonString);
             var user = users.Find(u => (u.Username == EmailOrUsername || u.Email == EmailOrUsername) && u.Password == Password);
-            if(user == null)
+
+            ChatModel chat = new ChatModel(1, DateTime.Now, false, 
+                new User("OleksaLviv", "OleksaLviv.png"),
+                [
+
+                    new MessageModel(1, 1, 1, false, "хєхє", DateTime.Now,  true, false),
+                    new MessageModel(2, 1, 2, true, "привіт", DateTime.Now, true, false),
+                    new MessageModel(2, 1, 2, true, "привіт", DateTime.Now, true, false),
+
+                ]);
+
+
+            JsonSerializer.Serialize(chat);
+
+            if (user == null)
             {
                 MessageBox.Show("Incorrect username/email or password");
                 return;
