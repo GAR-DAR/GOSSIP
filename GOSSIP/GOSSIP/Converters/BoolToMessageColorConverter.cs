ec3b352a3d3c09e3b65ddb99f499fb4714fe0b7e
pupsaik <151430709+pupsaik@users.noreply.github.com>
2024-11-23 15:52:12 +0200

    Message Text Color Change

---
+++ GOSSIP/GOSSIP/Converters/BoolToMessageColorConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace GOSSIP.Converters
+{
+    public class BoolToMessageColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool isSentByCurrentUser)
+            {
+                return isSentByCurrentUser ? "#000000" : "#FFFFFF";
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
