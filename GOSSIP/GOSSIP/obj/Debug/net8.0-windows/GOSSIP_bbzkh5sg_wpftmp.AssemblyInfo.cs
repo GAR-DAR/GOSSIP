700cdc35e25b0b0027d2942a603a213c7d97b4a2
IvanOlliinyk <144712641+IvanOlliinyk@users.noreply.github.com>
2024-11-23 22:28:22 +0200

    1

--- GOSSIP/GOSSIP/obj/Debug/net8.0-windows/GOSSIP_bbzkh5sg_wpftmp.AssemblyInfo.cs
+++ GOSSIP/GOSSIP/obj/Debug/net8.0-windows/GOSSIP_bbzkh5sg_wpftmp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GOSSIP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+669f6624d318e2e27fe5a538ca2980f177c50b17")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17fd0fcfb3fb1ea7008071eb249eaa3cf08af584")]
 [assembly: System.Reflection.AssemblyProductAttribute("GOSSIP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GOSSIP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- GOSSIP/GOSSIP/obj/Debug/net8.0-windows/Views/SignUpViewFirst.g.i.cs
+++ GOSSIP/GOSSIP/obj/Debug/net8.0-windows/Views/SignUpViewFirst.g.i.cs
@@ -53,7 +53,7 @@ namespace GOSSIP.Views {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/GOSSIP;V1.0.0.0;component/views/signupviewfirst.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/GOSSIP;component/views/signupviewfirst.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\SignUpViewFirst.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
