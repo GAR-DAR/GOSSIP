17fd0fcfb3fb1ea7008071eb249eaa3cf08af584
IvanOlliinyk <144712641+IvanOlliinyk@users.noreply.github.com>
2024-11-23 22:27:08 +0200

    Authorized Top Bar

--- GOSSIP/GOSSIP/obj/Debug/net8.0-windows/Views/LogInWindow.g.i.cs
+++ GOSSIP/GOSSIP/obj/Debug/net8.0-windows/Views/LogInWindow.g.i.cs
@@ -54,7 +54,7 @@ namespace GOSSIP.Views {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/GOSSIP;V1.0.0.0;component/views/loginwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/GOSSIP;component/views/loginwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\LogInWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
