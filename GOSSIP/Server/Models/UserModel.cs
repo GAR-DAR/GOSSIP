9928f1f2b87b1478b1a30e7787ce1bd71b9bac7e
Andrii <andriipotikha05@gmail.com>
2024-12-08 18:58:03 +0200

    update user model

--- GOSSIP/Server/Models/UserModel.cs
+++ GOSSIP/Server/Models/UserModel.cs
@@ -23,5 +23,7 @@ namespace Server.Models
         public bool IsBanned { get; set; }
         public string? Photo { get; set; }
         public List<ChatModel>? Chats { get; set; }
+        public Dictionary<uint, int> TopicVotes { get; set; } = [];
+        public Dictionary<uint, int> ReplyVotes { get; set; } = [];
     }
 }
