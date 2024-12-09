32f8821a35aaf3a4baf12eb89606d885f2d69019
pupsaik <151430709+pupsaik@users.noreply.github.com>
2024-12-10 00:33:50 +0200

    Search Beggining

--- GOSSIP/GOSSIP/ViewModels/AddUsersToChatVM.cs
+++ GOSSIP/GOSSIP/ViewModels/AddUsersToChatVM.cs
@@ -1,61 +1,112 @@
 ﻿using GOSSIP.JsonHandlers;
 using GOSSIP.Models;
-using GOSSIP.ViewModels;
-using GOSSIP;
+using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Windows.Input;
+using System.Linq;
 using System.Windows;
+using System.Windows.Input;
 
-public class AddUsersToChatVM : ObservableObject
+namespace GOSSIP.ViewModels
 {
-    public ICommand CloseCommand { get; set; }
-    public ICommand CreateChatCommand { get; }
-
-    // Подія для запиту на закриття вікна
-    public event Action<bool?> RequestClose;
-
-    public ObservableCollection<UserVM> AllUsers { get; set; }
-    public List<UserVM> SelectedUsers { get; set; } = new();
-
-    public AddUsersToChatVM()
+    public class AddUsersToChatVM : ObservableObject
     {
-        JsonStorage jsonStorage = new("user_data.json");
-        AllUsers = new ObservableCollection<UserVM>(
-            jsonStorage.LoadUsers()
-                       .Select(x => new UserVM(x))
-                       .Where(x => x.UserModel.ID != MainVM.AuthorizedUserVM.UserModel.ID));
+        public ICommand CloseCommand { get; set; }
+        public ICommand CreateChatCommand { get; }
 
-        CreateChatCommand = new RelayCommand(CreateChatMethod);
-        CloseCommand = new RelayCommand(obj => RequestClose?.Invoke(false));
-    }
+        // Подія для запиту на закриття вікна
+        public event Action<bool?> RequestClose;
 
-    private void CreateChatMethod(object obj)
-    {
-        if (SelectedUsers.Count == 0)
+        public ObservableCollection<UserVM> AllUsers { get; set; }
+        public ObservableCollection<UserVM> FilteredUsers { get; set; } = new();
+        public List<UserVM> SelectedUsers { get; set; } = new();
+
+        private string _searchText;
+        public string SearchText
         {
-            MessageBox.Show("Select at least one user to create a chat");
-            return;
+            get => _searchText;
+            set
+            {
+                if (_searchText != value)
+                {
+                    _searchText = value;
+                    OnPropertyChanged(nameof(SearchText));
+                    FilterUsers();
+                }
+            }
         }
 
-        string nameOfChat = MainVM.AuthorizedUserVM.Username + ", " + string.Join(", ", SelectedUsers.Select(user => user.Username));
-        if (nameOfChat.Length > 40)
+        private bool _isPopupOpen;
+        public bool IsPopupOpen
         {
-            nameOfChat = nameOfChat.Substring(0, 40);
+            get => _isPopupOpen;
+            set
+            {
+                if (_isPopupOpen != value)
+                {
+                    _isPopupOpen = value;
+                    OnPropertyChanged(nameof(IsPopupOpen));
+                }
+            }
         }
 
-        List<UserModel> users = [MainVM.AuthorizedUserVM.UserModel ];
-        users.AddRange(SelectedUsers.Select(x => x.UserModel));
+        public AddUsersToChatVM()
+        {
+            JsonStorage jsonStorage = new("user_data.json");
+            AllUsers = new ObservableCollection<UserVM>(
+                jsonStorage.LoadUsers()
+                           .Select(x => new UserVM(x))
+                           .Where(x => x.UserModel.ID != MainVM.AuthorizedUserVM.UserModel.ID));
 
-        ChatModel newChat = new(
-            0,
-            users,
-            nameOfChat,
-            DateTime.Now,
-            false,
-            []
+            CreateChatCommand = new RelayCommand(CreateChatMethod);
+            CloseCommand = new RelayCommand(obj => RequestClose?.Invoke(false));
+        }
+
+        private void FilterUsers()
+        {
+            if (string.IsNullOrWhiteSpace(SearchText))
+            {
+                FilteredUsers = new ObservableCollection<UserVM>();
+                IsPopupOpen = false;
+            }
+            else
+            {
+                var lowerSearchText = SearchText.ToLower();
+                FilteredUsers = new ObservableCollection<UserVM>(
+                    AllUsers.Where(user => user.Username.ToLower().Contains(lowerSearchText)));
+
+                IsPopupOpen = FilteredUsers.Any();
+            }
+            OnPropertyChanged(nameof(FilteredUsers));
+        }
+
+        private void CreateChatMethod(object obj)
+        {
+            if (SelectedUsers.Count == 0)
+            {
+                MessageBox.Show("Select at least one user to create a chat");
+                return;
+            }
+
+            string nameOfChat = MainVM.AuthorizedUserVM.Username + ", " + string.Join(", ", SelectedUsers.Select(user => user.Username));
+            if (nameOfChat.Length > 40)
+            {
+                nameOfChat = nameOfChat.Substring(0, 40);
+            }
+
+            List<UserModel> users = [MainVM.AuthorizedUserVM.UserModel, .. SelectedUsers.Select(x => x.UserModel)];
+
+            ChatModel newChat = new(
+                0,
+                users,
+                nameOfChat,
+                DateTime.Now,
+                false,
+                new List<MessageModel>()
             );
 
-        MainVM.AuthorizedUserVM.UserModel.Chats.Add(newChat);
-        RequestClose?.Invoke(true);
+            MainVM.AuthorizedUserVM.UserModel.Chats.Add(newChat);
+            RequestClose?.Invoke(true);
+        }
     }
 }
